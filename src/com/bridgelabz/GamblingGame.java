911a13326298ea0b766b29b9f7827eca31668350
Sam5196 <sammedpachore1008@gmail.com>
2022-06-18 17:30:54 +0530

    Added:Gambler either win or loose

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -4,6 +4,15 @@ public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Game..........!");
         int Gamebet = 1, Daystake = 100;
+        int game = (int) (Math.random() * 2);
+        if (game == 1) {
+            Daystake += Gamebet;
+            System.out.println("Win game = " + Daystake);
+        } else {
+            Daystake -= Gamebet;
+            System.out.println("Loose game = " + Daystake);
+        }
+    }
     }
 }
 
