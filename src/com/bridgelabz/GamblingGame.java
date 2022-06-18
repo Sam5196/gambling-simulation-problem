d8e664f5eea5f737fa7b2aecae72638f6b070db4
Sam5196 <sammedpachore1008@gmail.com>
2022-06-18 17:22:59 +0530

    Welcome to Gambling Simulation Game

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -3,15 +3,6 @@ package com.bridgelabz;
 public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Game..........!");
-
-        int Gamebet = 1, Daystake = 100;
-        int game = (int) (Math.random() * 2);
-        if (game == 1) {
-            Daystake += Gamebet;
-            System.out.println("Win game = " + Daystake);
-        } else {
-            Daystake -= Gamebet;
-            System.out.println("Loose game = " + Daystake);
     }
 }
 
