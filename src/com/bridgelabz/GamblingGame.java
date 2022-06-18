41690b5ebb93a0ae6486662626423e549288689c
Sam5196 <sammedpachore1008@gmail.com>
2022-06-18 17:27:03 +0530

    Added:Start Game

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Game..........!");
+        int Gamebet = 1, Daystake = 100;
     }
 }
 
