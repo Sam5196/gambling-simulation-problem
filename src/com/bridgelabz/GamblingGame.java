b785fbe6372cf23f072b2161f2382826e6cf2c4f
Sam5196 <sammedpachore1008@gmail.com>
2022-06-18 17:54:14 +0530

    Added:Resign for the day

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -1,18 +1,31 @@
 package com.bridgelabz;
 
 public class GamblingGame {
+    public static final int Gamebet = 1;
+    public static int Daystake = 100;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Gambling Simulation Game..........!");
-        int Gamebet = 1, Daystake = 100;
-        int game = (int) (Math.random() * 2);
-        if (game == 1) {
-            Daystake += Gamebet;
-            System.out.println("Win game = " + Daystake);
-        } else {
-            Daystake -= Gamebet;
-            System.out.println("Loose game = " + Daystake);
+        System.out.println("Welcome to Gambling Simulation Game.............!");
+        while (true) {
+            for (int i = 0; i < 100; i++) {
+
+                int game = (int) (Math.random() * 2);
+                if (game == 1) {
+                    Daystake += Gamebet;
+                } else {
+                    Daystake -= Gamebet;
+                }
+            }
+
+            if (Daystake >= 150) {
+                System.out.println("Win 50%:Resign the day");
+                break;
+            }
+            if (Daystake == 50) {
+                System.out.println("Loose 50%:Resign the day");
+                break;
+            }
         }
-    }
     }
 }
 
